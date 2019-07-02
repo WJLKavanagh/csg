e2d7a2ccc33c1ff0b571a233102545d1cfea821b
William_Kavanagh <bkav93@gmail.com>
2019-07-02 15:40:29 +0100

    analyse by meta changed to fair/interesting

--- 5char_csg/analyse_result_by_meta.py
+++ 5char_csg/analyse_result_by_meta.py
@@ -79,6 +79,7 @@ for line in result[34:]:
 for i in range(5):
     char_metric_dictionary[chars[i]]["end_result"] = float((result[i-5]).split(" ")[-1])
     char_metric_dictionary[chars[i]]["pick_rate"] = 0.0
+    char_metric_dictionary[chars[i]]["change_rate"] = 0.0
 
 for i in range(10):
     metric_dictionary[pairs[i]]["end_result"] = float((result[i-17]).split("\t")[-1])
@@ -113,32 +114,48 @@ for line in result[1:]:
         for char in chars:
             if success_dictionary[char] > 0:
                 pick_rates[char] += [success_dictionary[char] / total_winning_probability]
+            else:
+                pick_rates[char] += [0.0]
         total_winning_probability = 0
         success_dictionary = {"K":0, "A":0, "W":0, "R":0, "H":0}
 
 for char in chars:
-    sum = 0
-    for item in pick_rates[char]:
-        sum += item
-    char_metric_dictionary[char]["pick_rate"] = sum / len(pick_rates[char])
+    # identify loop start
+    # cut list at end point of loop start.
+    char_metric_dictionary[char]["pick_rate"] = np.mean(pick_rates[char][:33])
 
+change_rates = {"K":[], "A":[], "W":[], "R":[], "H":[]}
+
+for i in range(len(result[1:])):
+    if "were seen and" in result[i]:
+        c_p = result[i-1][:2]   # pull character pair
+        if float(result[i].split(" ")[-3]) > 0.0:
+            prop = float(result[i].split(" ")[-3]) / float(result[i].split(" ")[-7])
+        else:
+            prop = 0.0
+        change_rates[c_p[0]] += [prop/4]
+        change_rates[c_p[1]] += [prop/4]
+
+
+for char in chars:
+    char_metric_dictionary[char]["change_rate"] = np.mean(change_rates[char])
 
 char_dict = {"K":"Knight","A":"Archer","W":"Wizard","R":"Rogue","H":"Healer"}
 
-# now plot both limits against eachother
-ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
-ax.set_aspect('equal')
-ax.set_xlim(lims)
-ax.set_ylim(lims)
-ax.set_title("Pair-wise metagame analysis: " + sys.argv[1].split("/")[1].split(".txt")[0])
-ax.legend()
-plt.xlabel("robustness")
-plt.ylabel("win rate")
-plt.tight_layout()
+# # now plot both limits against eachother
+# ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
+# ax.set_aspect('equal')
+# ax.set_xlim(lims)
+# ax.set_ylim(lims)
+# ax.set_title("Pair-wise metagame analysis: " + sys.argv[1].split("/")[1].split(".txt")[0])
+# ax.legend()
+# plt.xlabel("robustness")
+# plt.ylabel("win rate")
+# plt.tight_layout()
 
 fig, ax2 = plt.subplots()
 for char in chars:
-    ax2.scatter(char_metric_dictionary[char]["pick_rate"], char_metric_dictionary[char]["win_delta"], label=char_dict[char], marker = 5, s=75)
+    ax2.scatter(char_metric_dictionary[char]["pick_rate"], char_metric_dictionary[char]["change_rate"], label=char_dict[char], marker = 5, s=75)
 ax2.legend()
 
 # Draw f(x) = y
@@ -154,17 +171,17 @@ ax2.legend()
 ax2.set_title("Character-wise metagame analysis: " + sys.argv[1].split("/")[1].split(".txt")[0])
 
 ax2.legend()
-plt.xlabel("pick rate")
-plt.ylabel("win delta")
+plt.xlabel("Fairness - character viability")
+plt.ylabel("Interest - strategy variation")
 plt.tight_layout()
 
-ind = np.arange(len(chars))
-fig, ax3 = plt.subplots()
-max_it = plt.plot([0,1,2,3,4], [len(meta_values)]*5, "--")
-base = plt.bar(ind, char_meta_count)
-extra = plt.bar(ind, char_near_meta_count, bottom = char_meta_count)
-plt.legend((base[0],extra[0]),("on meta","near meta (3%)"))
-plt.xticks(ind, chars)
-ax3.annotate(xy=[2, len(meta_values)], s = "iterations = " + str(len(meta_values)))
-ax3.set_title("Character-wise 'on meta'/'near meta' analysis")
+# ind = np.arange(len(chars))
+# fig, ax3 = plt.subplots()
+# max_it = plt.plot([0,1,2,3,4], [len(meta_values)]*5, "--")
+# base = plt.bar(ind, char_meta_count)
+# extra = plt.bar(ind, char_near_meta_count, bottom = char_meta_count)
+# plt.legend((base[0],extra[0]),("on meta","near meta (3%)"))
+# plt.xticks(ind, chars)
+# ax3.annotate(xy=[2, len(meta_values)], s = "iterations = " + str(len(meta_values)))
+# ax3.set_title("Character-wise 'on meta'/'near meta' analysis")
 plt.show()
