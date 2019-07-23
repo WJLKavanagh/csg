40fc79178e99a7c19184d4822e40af2a4858c1ea
William Kavanagh <bkav93@gmail.com>
2019-07-23 17:30:51 +0100

    cool new visualisations

---
+++ 5char_csg/chord_practice.py
@@ -0,0 +1,63 @@
+import re
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+results = {'KA': {'res': '0.217740464642635', 'opp': 'WH'}, 'KW': {'res': '0.36400061678485385', 'opp': 'WH'}, 'KR': {'res': '0.27324713957749186', 'opp': 'WR'}, 'KH': {'res': '0.2895677705543638', 'opp': 'WH'}, 'AW': {'res': '0.2833221421023975', 'opp': 'WH'}, 'AR': {'res': '0.17039787458962413', 'opp': 'WH'}, 'AH': {'res': '0.2586919097453748', 'opp': 'WH'}, 'WR': {'res': '0.38426962625438743', 'opp': 'AW'}, 'WH': {'res': '0.44862622066492686', 'opp': 'WR'}, 'RH': {'res': '0.33647407987116457', 'opp': 'WR'}}
+
+plt.subplots(figsize=(14,14))
+
+G = nx.DiGraph()
+
+evil_nodes = []
+good_nodes = []
+
+for p in results.keys():
+    if float(results[p]["res"]) < 0.499 or p != results[p]["opp"]:
+        G.add_edge(results[p]["opp"], p, weight= str(100 - float(results[p]["res"])*100)[:6])
+    else:
+        evil_nodes += [p]
+        G.add_edge(p, p, weight= 0.5)
+        print("Adding:",p)
+plt.plot()
+
+#G.set_node_attributes(G, labels, 'colour_group')
+if len(evil_nodes) == 0:
+    try:
+        nx.find_cycle(G)
+        if len(nx.find_cycle(G)) > 1:
+            for e in nx.find_cycle(G):
+                good_nodes += [e[0]]
+    except:
+        pass
+
+print(good_nodes, evil_nodes)
+
+colour_list = []
+for n in G.nodes():
+    if n in good_nodes or n in evil_nodes:
+        colour_list += [0]
+    else:
+        colour_list += [1]
+
+print(colour_list)
+
+carac = pd.DataFrame({'ID':G.nodes(), 'colour_group':colour_list})
+carac= carac.set_index('ID')
+carac=carac.reindex(G.nodes())
+carac['colour_group']=pd.Categorical(carac['colour_group'])
+carac['colour_group'].cat.codes
+
+edge_labels=dict([((u,v,),d['weight'])
+             for u,v,d in G.edges(data=True)])
+pos = nx.shell_layout(G)
+nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=carac['colour_group'].cat.codes, cmap=plt.cm.Set1, draw_network_edge_labels = True, node_size=800)
+
+print("edges", G.edges())
+print("cycle", nx.find_cycle(G))
+
+
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+plt.axis('off')
+#plt.savefig("results/graphics/" + config + "_optimality_relationship.png")
+plt.show()
--- 5char_csg/search_for_dominance.py
+++ 5char_csg/search_for_dominance.py
@@ -1,6 +1,9 @@
 # William Kavanagh, May 2019
 # Extended CSG - Can we identify dominant strategies without iterating?
-
+import re
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 import sys, os, datetime
 import metagenerator, strategy_generator, model_generator, strategy_updater, strategy_reverse, final_material_comp
 
@@ -28,6 +31,7 @@ pairs = []
 for i in range(len(chars)):
     for j in range(i+1,len(chars)):
         pairs += [chars[i] + chars[j]]
+results = {}
 
 for pair in pairs:
     # generate model for pair vs generator
@@ -64,57 +68,68 @@ for pair in pairs:
     "/tmp.sta -javamaxmem 5g -nopre -maxiters 300000 > " + output + "/log.txt")
 
     print(pair + ":")
-    print("\topposing pair selected as: " + find_opponents(output+"/tmp.tra"))
-    print("\toptimal probability of: " + find_result(output+"/log.txt") + "\n")
+    found_opp = find_opponents(output+"/tmp.tra")
+    minimax = find_result(output+"/log.txt")
+    print("\topposing pair selected as: " + found_opp)
+    print("\toptimal probability of: " + minimax + "\n")
+    results[pair] = {"res":minimax, "opp":found_opp}
 
+
+plt.subplots(figsize=(14,14))
+
+G = nx.DiGraph()
+
+evil_nodes = []
+good_nodes = []
+
+for p in results.keys():
+    if float(results[p]["res"]) < 0.499 or p != results[p]["opp"]:
+        G.add_edge(results[p]["opp"], p, weight= str(100 - float(results[p]["res"])*100)[:6])
+    else:
+        evil_nodes += [p]
+        G.add_edge(p, p, weight= 0.5)
+        print("Adding:",p)
+plt.plot()
+
+#G.set_node_attributes(G, labels, 'colour_group')
+if len(evil_nodes) == 0:
+    try:
+        nx.find_cycle(G)
+        if len(nx.find_cycle(G)) > 1:
+            for e in nx.find_cycle(G):
+                good_nodes += [e[0]]
+    except:
+        pass
+
+print(good_nodes, evil_nodes)
+
+colour_list = []
+for n in G.nodes():
+    if n in good_nodes or n in evil_nodes:
+        colour_list += [0]
+    else:
+        colour_list += [1]
+
+print(colour_list)
+
+carac = pd.DataFrame({'ID':G.nodes(), 'colour_group':colour_list})
+carac= carac.set_index('ID')
+carac=carac.reindex(G.nodes())
+carac['colour_group']=pd.Categorical(carac['colour_group'])
+carac['colour_group'].cat.codes
+
+edge_labels=dict([((u,v,),d['weight'])
+             for u,v,d in G.edges(data=True)])
+pos = nx.shell_layout(G)
+nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=carac['colour_group'].cat.codes, cmap=plt.cm.Set1, draw_network_edge_labels = True, node_size=800)
+
+print("edges", G.edges())
+print("cycle", nx.find_cycle(G))
+
+
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+plt.axis('off')
+plt.savefig("results/graphics/" + config + "_optimality_relationship.png")
+plt.show()
     # p1 can do what they want, as can p2.
     # calculate the optimal strategy for P1, print P and the opposing pair.
-
-
-
-
-
-#
-#     os.system("prism output/" + pair + "_generator.prism ../properties/mdp.props \
-#     -prop 1 -exportadvmdp " + output + "/tmp.tra -exportstates " + output + \
-#     "/tmp.sta -javamaxmem 5g -nopre -maxiters 300000 > " + output + "/log.txt")
-#     probability = float(find_result(output+"/log.txt"))
-#     print(pair + " can get " + str(probability))
-#     if probability > best_probability:
-#         meta_pair = pair
-#         best_probability = probability
-#     transition_count = strategy_generator.run(output, pair, k)
-#     print("Strategy generated, there are: " + str(transition_count) + " transitions.\n")
-# print(meta_pair + " is the meta after " + str(k) + " iterations.\n~~~~~~~~~~~~~~~~")
-# k += 1
-#
-# best_probability = 0.0
-# while loop_check(k) == 0 or (best_probability > 0.5 and loop_check(k) == 1):
-#     best_probability = 0.0
-#     best_pair_this_it = "none"
-#     model_generator.run(output, config, meta_pair, k)       # generate the 10 prism models vs current meta
-#     for pair in pairs:
-#         # find the probability of winning
-#         os.system("prism output/" + pair + "_vs_" + str(k-1) + ".prism ../properties/mdp.props \
-#         -prop 1 -exportadvmdp " + output + "/tmp.tra -exportstates " + output + \
-#         "/tmp.sta -javamaxmem 5g > " + output + "/log.txt")
-#         probability = float(find_result(output+"/log.txt"))
-#         print(pair + " can get " + str(probability))
-#         # designate the new meta
-#         if probability > best_probability:
-#             best_pair_this_it = pair
-#             best_probability = probability
-#         # generate the new strategies
-#         total, seen, new = strategy_updater.run(output, pair, k)
-#         # print how many transitions have been updated
-#         print("Of " + str(total) + " actions, " + str(seen) + " were seen and " + str(new) +  " have changed.\n")
-#     meta_pair = best_pair_this_it
-#     print(meta_pair + " is the meta after " + str(k) + " iterations.\n~~~~~~~~~~~~~~~~")
-#     k = k + 1
-# loop_size = loop_check(k)
-# if loop_size != 1: print("Dominant strategy identified")
-# else: print("Cycle found of length:", loop_size)
-# print("Generation finished, building final table")
-# for pair in pairs[1:]:
-#     strategy_reverse.run(pair, k-1, output)
-# final_material_comp.run(k-1, output, config)
